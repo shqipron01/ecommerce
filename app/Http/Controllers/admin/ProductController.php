4673b4646c768307e04036a3291178d4172e3e3a
shqipron01 <57071701+shqipron01@users.noreply.github.com>
2025-05-19 19:23:28 +0200

    Added product crud front

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\TempImage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ProductController extends Controller
 {   
@@ -52,6 +55,35 @@ class ProductController extends Controller
         $product->barcode = $request->barcode;
         $product->save();
 
+        if(!empty($request->gallery)){
+            foreach($request->gallery as $key => $tempImageId){
+                $tempImage = TempImage::find($tempImageId);
+                
+                //Large thumbnail
+
+                $extArray = explode('.', $tempImage->name);
+                $ext = end($extArray);
+
+                $imageName = $product->id. '-' .time(). '.' .$ext;
+                
+                $manager = new ImageManager(Driver::class);
+                $img = $manager->read(public_path('uploads/temp/'. $tempImage->name));
+                $img->scaleDown(1200);
+                $img->save(public_path('uploads/products/large/'. $imageName));
+
+                //Small thumbnail
+                $manager = new ImageManager(Driver::class);
+                $img = $manager->read(public_path('uploads/temp/'. $tempImage->name));
+                $img->coverDown(400, 460);
+                $img->save(public_path('uploads/products/small/'. $imageName));
+
+                if($key == 0){
+                    $product->image = $imageName;
+                    $product->save();
+                }
+            }
+        }
+
         return response()->json([
             'status' => 200,
             'message' => 'Product added successfully'
@@ -60,14 +92,82 @@ class ProductController extends Controller
     }
     //This method will return a single product
     public function show($id){
+        $product = Product::find($id);
 
+        if($product == null){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found'
+            ],404);
+        }
+
+        return response()->json([
+            'status'=> 200,
+            'data'=> $product
+        ],200);
     }
     //This method will update a product
-    public function update($id){
+    public function update($id, Request $request){
+        $product = Product::find($id);
 
+        if($product == null){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found',
+            ],404);
+        }
+
+        //Validate the request
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|integer',
+            'sku' => 'required|unique:products,sku, '.$id. ',id',
+            'is_featured' => 'required',
+            'status' => 'required',
+        ]);
+        //Check if validation fails
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ],400);
+        }
+        //Update the product
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category;
+        $product->brand_id = $request->brand;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->barcode = $request->barcode;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been updated successfully'
+        ],200);
     }
     //This method will delete a product
     public function destroy($id){
+        $product = Product::find($id);
 
+        if($product == null){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found'
+            ],404);
+        }
+        $product->delete();
+
+        return response()->json([
+            'status'=> 200,
+            'message'=> 'Product has been deleted successfully'
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\SizeController;
+use App\Http\Controllers\admin\TempImageController;
 use App\Models\Size;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,8 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::resource('brands', BrandController::class);
     Route::get('sizes',[SizeController::class, 'index']);
     Route::resource('products', ProductController::class);
+    Route::post('temp-images',[TempImageController::class, 'store']);
+    
 })
 
 ?>
