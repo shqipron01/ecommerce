3ad067fcce8a37c4315bc68c6eda00b0f56749c8
arlindkurti <arlind4002@gmail.com>
2025-05-20 10:34:46 +0200

    Admin

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductImage;
 use App\Models\TempImage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
@@ -76,6 +77,11 @@ class ProductController extends Controller
                 $img = $manager->read(public_path('uploads/temp/'. $tempImage->name));
                 $img->coverDown(400, 460);
                 $img->save(public_path('uploads/products/small/'. $imageName));
+
+                $productImage = new ProductImage();
+                $productImage->image = $imageName;
+                $productImage->product_id = $product->id;
+                $productImage->save();
 
                 if($key == 0){
                     $product->image = $imageName;
